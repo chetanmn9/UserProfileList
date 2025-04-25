22f9fa39ee692256693c43beaa8ffff9a9c72c25
Chetan <chetan.mn9@gmail.com>
2025-04-25 21:08:56 +1000

    Initial Commit

---
+++ UserList/UserListApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserListApp.swift
+//  UserList
+//
+//  Created by Chetan on 25/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct UserListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UserListTests/UserListTests.swift
@@ -0,0 +1,17 @@
+//
+//  UserListTests.swift
+//  UserListTests
+//
+//  Created by Chetan on 25/4/2025.
+//
+
+import Testing
+@testable import UserList
+
+struct UserListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
